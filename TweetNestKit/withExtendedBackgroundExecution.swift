c5c0b3040cfe85fd1121d4916ce3c895d1d99398
Jaehong Kang <sinoru@me.com>
2021-08-31 16:59:27 +0900

    Adds log

--- TweetNestKit/withExtendedBackgroundExecution.swift
+++ TweetNestKit/withExtendedBackgroundExecution.swift
@@ -6,9 +6,12 @@
 //
 
 import Foundation
+import UnifiedLogging
 
 public func withExtendedBackgroundExecution<T>(identifier: String = #function, body: () async throws -> T) async rethrows -> T {
     #if !os(macOS)
+    let logger = Logger(subsystem: Bundle.module.bundleIdentifier!, category: "process-activity")
+    
     let taskSemaphore = DispatchSemaphore(value: 0)
     let cancellationSemaphore = DispatchSemaphore(value: 0)
     defer {
@@ -19,10 +22,15 @@ public func withExtendedBackgroundExecution<T>(identifier: String = #function, b
     withUnsafeCurrentTask { task in
         ProcessInfo.processInfo.performExpiringActivity(withReason: identifier) { expired in
             if expired {
+                logger.notice("\(identifier): Canceling process activity")
                 task?.cancel()
+                logger.notice("\(identifier): Waiting for process activity cancelled")
                 cancellationSemaphore.wait()
+                logger.notice("\(identifier): Process activity cancelled")
             } else {
+                logger.notice("\(identifier): Waiting for process activity done")
                 taskSemaphore.wait()
+                logger.notice("\(identifier): Process activity done")
             }
         }
     }
@@ -33,5 +41,6 @@ public func withExtendedBackgroundExecution<T>(identifier: String = #function, b
     }
     #endif
     
+    logger.notice("\(identifier): Starting process activity")
     return try await body()
 }
