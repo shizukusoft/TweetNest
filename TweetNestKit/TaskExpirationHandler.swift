e20da8e7cb1bb690a631ed04b21374ef38864f32
Jaehong Kang <sinoru@me.com>
2021-09-03 12:27:03 +0900

    Refactoring TaskExpirationHandler

--- TweetNestKit/TaskExpirationHandler.swift
+++ TweetNestKit/TaskExpirationHandler.swift
@@ -8,70 +8,66 @@
 import Foundation
 
 public func withTaskExpirationHandler<T>(body: @escaping (@escaping @Sendable () -> Void) async -> T) async -> T {
-    return await TaskExpirationHandler<T, Never>().run(body: body)
+    return await TaskExpirationHandler().run(body: body)
 }
 
 public func withTaskExpirationHandler<T>(body: @escaping (@escaping @Sendable () -> Void) async throws -> T) async throws -> T {
-    return try await TaskExpirationHandler<T, Error>().run(body: body)
+    return try await TaskExpirationHandler().run(body: body)
 }
 
-private actor TaskExpirationHandler<Success, Failure> where Failure: Error {
-    private let cancellationSemaphore = DispatchSemaphore(value: 0)
-    private var task: Task<Success, Failure>?
-
-    func updateTask(_ task: Task<Success, Failure>?) {
-        self.task = task
-    }
+private actor TaskExpirationHandler {
+    private let dispatchGroup = DispatchGroup()
+    private var cancellationHandler: (() -> ())?
 }
 
 extension TaskExpirationHandler {
     @Sendable
     nonisolated func cancel() {
         Task.detached(priority: .high) {
-            await self.task?.cancel()
+            await self.cancellationHandler?()
         }
-        cancellationSemaphore.wait()
+        dispatchGroup.wait()
     }
 }
 
-extension TaskExpirationHandler where Failure == Error {
-    @Sendable
-    nonisolated func run(body: @escaping (@escaping @Sendable () -> Void) async throws -> Success) async throws -> Success {
-        let task = Task<Success, Error> {
+extension TaskExpirationHandler {
+    func run<T>(body: @escaping (@escaping @Sendable () -> Void) async throws -> T) async throws -> T {
+        dispatchGroup.enter()
+        let task = Task<T, Error> {
             defer {
-                cancellationSemaphore.signal()
+                dispatchGroup.leave()
             }
 
             return try await body(self.cancel)
         }
 
-        await updateTask(task)
-
         return try await withTaskCancellationHandler {
+            self.cancellationHandler = task.cancel
+
             return try await task.value
         } onCancel: {
-            cancel()
+            task.cancel()
         }
     }
 }
 
-extension TaskExpirationHandler where Failure == Never {
-    @Sendable
-    nonisolated func run(body: @escaping (@escaping @Sendable () -> Void) async -> Success) async -> Success {
-        let task = Task<Success, Never> {
+extension TaskExpirationHandler {
+    func run<T>(body: @escaping (@escaping @Sendable () -> Void) async -> T) async -> T {
+        dispatchGroup.enter()
+        let task = Task<T, Never> {
             defer {
-                cancellationSemaphore.signal()
+                dispatchGroup.leave()
             }
 
             return await body(self.cancel)
         }
 
-        await updateTask(task)
-
         return await withTaskCancellationHandler {
+            self.cancellationHandler = task.cancel
+
             return await task.value
         } onCancel: {
-            cancel()
+            task.cancel()
         }
     }
 }
