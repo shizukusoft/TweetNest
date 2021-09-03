7ad69a42f734b573e2d177fe239584ff4afacf0a
Jaehong Kang <sinoru@me.com>
2021-09-03 22:54:53 +0900

    Refactoring on withExtendedBackgroundExecution

--- TweetNestKit/BackgroundTasks.swift
+++ TweetNestKit/BackgroundTasks.swift
@@ -8,52 +8,45 @@
 import Foundation
 import UnifiedLogging
 
-#if os(macOS)
 @inlinable
-public func withExtendedBackgroundExecution<T>(function: String = #function, fileID: String = #fileID, line: Int = #line, body: @escaping () async throws -> T) async rethrows -> T {
-    try await withExtendedBackgroundExecution(identifier: "\(function) (\(fileID):\(line))", body: body)
+public func withExtendedBackgroundExecution<T>(function: String = #function, fileID: String = #fileID, line: Int = #line, expirationHandler: (() -> ())? = nil, body: () throws -> T) rethrows -> T {
+    try withExtendedBackgroundExecution(identifier: "\(function) (\(fileID):\(line))", expirationHandler: expirationHandler, body: body)
 }
 
-public func withExtendedBackgroundExecution<T>(identifier: String, body: @escaping () async throws -> T) async rethrows -> T {
+public func withExtendedBackgroundExecution<T>(identifier: String, expirationHandler: (() -> ())? = nil, body: () throws -> T) rethrows -> T {
+    let logger = Logger(subsystem: Bundle.module.bundleIdentifier!, category: "process-activity")
+
+    #if os(macOS)
     let token = ProcessInfo.processInfo.beginActivity(options: .idleSystemSleepDisabled, reason: identifier)
     defer {
         ProcessInfo.processInfo.endActivity(token)
     }
-
-    logger.notice("\(identifier, privacy: .public): Start process activity")
+    #else
+    let dispatchGroup = DispatchGroup()
+    dispatchGroup.enter()
     defer {
-        logger.notice("\(identifier, privacy: .public): Process activity finished with cancelled: \(Task.isCancelled)")
-    }
-
-    return try await body()
-}
-#else
-private func handleExtendedBackgroundExecution<T>(identifier: String, expirationHandler: @escaping @Sendable () -> Void, body: @escaping () async throws -> T) async rethrows -> T {
-    let logger = Logger(subsystem: Bundle.module.bundleIdentifier!, category: "process-activity")
-
-    let taskSemaphore = DispatchSemaphore(value: 0)
-    defer {
-        taskSemaphore.signal()
+        dispatchGroup.leave()
     }
 
     ProcessInfo.processInfo.performExpiringActivity(withReason: identifier) { expired in
         if expired {
             logger.notice("\(identifier, privacy: .public): Cancel process activity")
-            expirationHandler()
+            expirationHandler?()
             logger.notice("\(identifier, privacy: .public): Cancelling Process activity finished")
         } else {
             logger.notice("\(identifier, privacy: .public): Wait process activity")
-            taskSemaphore.wait()
+            dispatchGroup.wait()
             logger.notice("\(identifier, privacy: .public): Waiting process activity finished")
         }
     }
+    #endif
 
     logger.notice("\(identifier, privacy: .public): Start process activity")
     defer {
         logger.notice("\(identifier, privacy: .public): Process activity finished with cancelled: \(Task.isCancelled)")
     }
 
-    return try await body()
+    return try body()
 }
 
 @inlinable
@@ -77,4 +70,39 @@ public func withExtendedBackgroundExecution<T>(identifier: String, body: @escapi
         return await handleExtendedBackgroundExecution(identifier: identifier, expirationHandler: expirationHandler, body: body)
     }
 }
-#endif
+
+private func handleExtendedBackgroundExecution<T>(identifier: String, expirationHandler: @escaping @Sendable () -> Void, body: @escaping () async throws -> T) async rethrows -> T {
+    let logger = Logger(subsystem: Bundle.module.bundleIdentifier!, category: "process-activity")
+
+    #if os(macOS)
+    let token = ProcessInfo.processInfo.beginActivity(options: .idleSystemSleepDisabled, reason: identifier)
+    defer {
+        ProcessInfo.processInfo.endActivity(token)
+    }
+    #else
+    let dispatchGroup = DispatchGroup()
+    dispatchGroup.enter()
+    defer {
+        dispatchGroup.leave()
+    }
+
+    ProcessInfo.processInfo.performExpiringActivity(withReason: identifier) { expired in
+        if expired {
+            logger.notice("\(identifier, privacy: .public): Cancel process activity")
+            expirationHandler()
+            logger.notice("\(identifier, privacy: .public): Cancelling Process activity finished")
+        } else {
+            logger.notice("\(identifier, privacy: .public): Wait process activity")
+            dispatchGroup.wait()
+            logger.notice("\(identifier, privacy: .public): Waiting process activity finished")
+        }
+    }
+    #endif
+
+    logger.notice("\(identifier, privacy: .public): Start process activity")
+    defer {
+        logger.notice("\(identifier, privacy: .public): Process activity finished with cancelled: \(Task.isCancelled)")
+    }
+
+    return try await body()
+}
