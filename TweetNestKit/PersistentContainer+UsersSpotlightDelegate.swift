4c4378570acc3847576e34f0d36e4225f9eb22ee
Jaehong Kang <sinoru@me.com>
2021-09-10 13:32:35 +0900

    Clean-up TweetNestKit target structure

---
+++ TweetNestKit/PersistentContainer+UsersSpotlightDelegate.swift
@@ -0,0 +1,50 @@
+//
+//  PersistentContainer+UsersSpotlightDelegate.swift
+//  PersistentContainer+UsersSpotlightDelegate
+//
+//  Created by Jaehong Kang on 2021/08/06.
+//
+
+#if canImport(CoreSpotlight)
+
+import CoreData
+import CoreSpotlight
+
+extension PersistentContainer {
+    class UsersSpotlightDelegate: NSCoreDataCoreSpotlightDelegate {
+        override func domainIdentifier() -> String {
+            "\(Bundle.tweetNestKit.bundleIdentifier!).users"
+        }
+
+        override func indexName() -> String? {
+            "users-index"
+        }
+
+        override func attributeSet(for object: NSManagedObject) -> CSSearchableItemAttributeSet? {
+            if let user = object as? User {
+                let attributeSet = CSSearchableItemAttributeSet(contentType: .contact)
+
+                let sortedUserDetails = user.sortedUserDetails
+
+                attributeSet.identifier = user.id
+                attributeSet.displayName = sortedUserDetails?.last?.name
+                attributeSet.alternateNames = sortedUserDetails?.last?.username.flatMap { ["@\($0)"] }
+                attributeSet.thumbnailData = try? sortedUserDetails?.last?.profileImageURL.flatMap {
+                    let fetchRequest = DataAsset.fetchRequest()
+                    fetchRequest.predicate = NSPredicate(format: "url == %@", $0 as NSURL)
+                    fetchRequest.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+                    fetchRequest.fetchLimit = 1
+
+                    return try user.managedObjectContext?.fetch(fetchRequest).first?.data
+                }
+                attributeSet.keywords = (sortedUserDetails?.compactMap(\.name) ?? []) + (sortedUserDetails?.compactMap(\.username) ?? [])
+
+                return attributeSet
+            }
+
+            return nil
+        }
+    }
+}
+
+#endif
