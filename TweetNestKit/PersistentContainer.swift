7549aede1831b5b7848872fed649fbe4a1565ba3
Jaehong Kang <sinoru@me.com>
2021-09-20 12:37:32 +0900

    Adds fatalError

--- TweetNestKit/PersistentContainer.swift
+++ TweetNestKit/PersistentContainer.swift
@@ -21,21 +21,36 @@ public class PersistentContainer: NSPersistentCloudKitContainer {
     private static let _managedObjectModel: NSManagedObjectModel = {
         let managedObjectModel = NSManagedObjectModel(contentsOf: Bundle.tweetNestKit.url(forResource: Bundle.tweetNestKit.name!, withExtension: "momd")!)!
 
-        let accountEntity = managedObjectModel.entitiesByName["Account"]
-        let usersFetchedPropertyDescription = accountEntity?.propertiesByName["users"] as? NSFetchedPropertyDescription
-        usersFetchedPropertyDescription?.fetchRequest?.sortDescriptors = [
+        guard let accountEntity = managedObjectModel.entitiesByName["Account"] else {
+            fatalError("Account entity not found.")
+        }
+
+        guard let usersFetchedPropertyDescription = accountEntity.propertiesByName["users"] as? NSFetchedPropertyDescription else {
+            fatalError("users property not found in Account entity.")
+        }
+
+        usersFetchedPropertyDescription.fetchRequest?.sortDescriptors = [
             NSSortDescriptor(keyPath: \User.creationDate, ascending: true),
             NSSortDescriptor(keyPath: \User.modificationDate, ascending: true),
         ]
 
-        let userEntity = managedObjectModel.entitiesByName["User"]
-        let accountsFetchedPropertyDescription = userEntity?.propertiesByName["accounts"] as? NSFetchedPropertyDescription
-        accountsFetchedPropertyDescription?.fetchRequest?.sortDescriptors = [
+        guard let userEntity = managedObjectModel.entitiesByName["User"] else {
+            fatalError("User entity not found.")
+        }
+
+        guard let accountsFetchedPropertyDescription = userEntity.propertiesByName["accounts"] as? NSFetchedPropertyDescription else {
+            fatalError("accounts property not found in User entity.")
+        }
+
+        accountsFetchedPropertyDescription.fetchRequest?.sortDescriptors = [
             NSSortDescriptor(keyPath: \Account.creationDate, ascending: true),
         ]
 
-        let sortedUserDetailsFetchedPropertyDescription = userEntity?.propertiesByName["sortedUserDetails"] as? NSFetchedPropertyDescription
-        sortedUserDetailsFetchedPropertyDescription?.fetchRequest?.sortDescriptors = [
+        guard let sortedUserDetailsFetchedPropertyDescription = userEntity.propertiesByName["sortedUserDetails"] as? NSFetchedPropertyDescription else {
+            fatalError("sortedUserDetails property not found in User entity.")
+        }
+
+        sortedUserDetailsFetchedPropertyDescription.fetchRequest?.sortDescriptors = [
             NSSortDescriptor(keyPath: \UserDetail.creationDate, ascending: true),
         ]
 
