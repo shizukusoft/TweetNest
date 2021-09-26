5ab48d2627c355b91f11c797b0684c8f17896dd4
Jaehong Kang <sinoru@me.com>
2021-09-26 12:18:13 +0900

    Enhances withExtendedBackgroundExecution async function

--- TweetNestKit/BackgroundTasks.swift
+++ TweetNestKit/BackgroundTasks.swift
@@ -49,13 +49,21 @@ public func withExtendedBackgroundExecution<T>(identifier: String, expirationHan
     return try body()
 }
 
+extension Task where Success == Never, Failure == Never {
+    @TaskLocal fileprivate static var isInExtendedBackgroundExecution: Bool = false
+}
+
 @inlinable
 public func withExtendedBackgroundExecution<T>(function: String = #function, fileID: String = #fileID, line: Int = #line, body: @escaping () async throws -> T) async throws -> T {
     try await withExtendedBackgroundExecution(identifier: "\(function) (\(fileID):\(line))", body: body)
 }
 
 public func withExtendedBackgroundExecution<T>(identifier: String, body: @escaping () async throws -> T) async throws -> T {
-    try await withTaskExpirationHandler { expirationHandler in
+    guard Task.isInExtendedBackgroundExecution == false else {
+        return try await body()
+    }
+
+    return try await withTaskExpirationHandler { expirationHandler in
         return try await handleExtendedBackgroundExecution(identifier: identifier, expirationHandler: expirationHandler, body: body)
     }
 }
@@ -66,43 +74,49 @@ public func withExtendedBackgroundExecution<T>(function: String = #function, fil
 }
 
 public func withExtendedBackgroundExecution<T>(identifier: String, body: @escaping () async -> T) async -> T {
-    await withTaskExpirationHandler { expirationHandler in
+    guard Task.isInExtendedBackgroundExecution == false else {
+        return await body()
+    }
+
+    return await withTaskExpirationHandler { expirationHandler in
         return await handleExtendedBackgroundExecution(identifier: identifier, expirationHandler: expirationHandler, body: body)
     }
 }
 
 private func handleExtendedBackgroundExecution<T>(identifier: String, expirationHandler: @escaping @Sendable () -> Void, body: @escaping () async throws -> T) async rethrows -> T {
-    let logger = Logger(subsystem: Bundle.tweetNestKit.bundleIdentifier!, category: "extended-background-execution")
+    try await Task.$isInExtendedBackgroundExecution.withValue(true) {
+        let logger = Logger(subsystem: Bundle.tweetNestKit.bundleIdentifier!, category: "extended-background-execution")
 
-    #if os(macOS)
-    let token = ProcessInfo.processInfo.beginActivity(options: [.idleSystemSleepDisabled, .suddenTerminationDisabled, .automaticTerminationDisabled], reason: identifier)
-    defer {
-        ProcessInfo.processInfo.endActivity(token)
-    }
-    #else
-    let dispatchGroup = DispatchGroup()
-    dispatchGroup.enter()
-    defer {
-        dispatchGroup.leave()
-    }
-
-    ProcessInfo.processInfo.performExpiringActivity(withReason: identifier) { expired in
-        if expired {
-            logger.notice("\(identifier, privacy: .public): Expiring activity expired")
-            expirationHandler()
-            logger.notice("\(identifier, privacy: .public): Expiring activity expirationHandler finished")
-        } else {
-            logger.info("\(identifier, privacy: .public): Start expiring activity")
-            dispatchGroup.wait()
-            logger.info("\(identifier, privacy: .public): Expiring activity finished")
+        #if os(macOS)
+        let token = ProcessInfo.processInfo.beginActivity(options: [.idleSystemSleepDisabled, .suddenTerminationDisabled, .automaticTerminationDisabled], reason: identifier)
+        defer {
+            ProcessInfo.processInfo.endActivity(token)
         }
-    }
-    #endif
+        #else
+        let dispatchGroup = DispatchGroup()
+        dispatchGroup.enter()
+        defer {
+            dispatchGroup.leave()
+        }
 
-    logger.notice("\(identifier, privacy: .public): Start")
-    defer {
-        logger.notice("\(identifier, privacy: .public): Finished with cancelled: \(Task.isCancelled)")
-    }
+        ProcessInfo.processInfo.performExpiringActivity(withReason: identifier) { expired in
+            if expired {
+                logger.notice("\(identifier, privacy: .public): Expiring activity expired")
+                expirationHandler()
+                logger.notice("\(identifier, privacy: .public): Expiring activity expirationHandler finished")
+            } else {
+                logger.info("\(identifier, privacy: .public): Start expiring activity")
+                dispatchGroup.wait()
+                logger.info("\(identifier, privacy: .public): Expiring activity finished")
+            }
+        }
+        #endif
 
-    return try await body()
+        logger.notice("\(identifier, privacy: .public): Start")
+        defer {
+            logger.notice("\(identifier, privacy: .public): Finished with cancelled: \(Task.isCancelled)")
+        }
+
+        return try await body()
+    }
 }
