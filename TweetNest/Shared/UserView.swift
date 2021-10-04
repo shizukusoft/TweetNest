5aa94402a13c2c85ce9cafdaa2cba71756b6ac41
Jaehong Kang <sinoru@me.com>
2021-10-04 13:30:19 +0900

    Revert "Optimise user profile URL usage"
    This reverts commit 8ce493cda1be8cb3be20f6596da776fd52962268.

--- TweetNest/Shared/UserView.swift
+++ TweetNest/Shared/UserView.swift
@@ -11,7 +11,6 @@ import UnifiedLogging
 
 struct UserView: View {
     let userID: String
-    let userProfileURL: URL
 
     var displayUserID: String {
         return Int64(userID).flatMap { "#\($0.twnk_formatted())" } ?? "#\(userID)"
@@ -46,6 +45,12 @@ struct UserView: View {
     @State var safariSheetURL: URL?
     @State var shareSheetURL: URL?
     #endif
+
+    var userProfileURL: URL? {
+        user?.id.flatMap {
+            URL(string: "https://twitter.com/intent/user?user_id=\($0)")!
+        }
+    }
 
     @State var showBulkDeleteRecentTweets: Bool = false
     #if os(iOS) || os(macOS)
@@ -160,20 +165,25 @@ struct UserView: View {
             #if os(iOS) || os(macOS)
             .toolbar {
                 ToolbarItemGroup(placement: .automatic) {
-                    let userProfileURL = self.userProfileURL
                     if shouldCompactToolbar {
                         Menu {
-                            Link(destination: userProfileURL) {
-                                Label("Open Profile", systemImage: "safari")
+                            if let userProfileURL = userProfileURL {
+                                Link(destination: userProfileURL) {
+                                    Label("Open Profile", systemImage: "safari")
+                                }
+
+                                #if os(iOS)
+                                Button {
+                                    safariSheetURL = userProfileURL
+                                } label: {
+                                    Label("Open Profile in Safari", systemImage: "safari")
+                                }
+                                #endif
                             }
 
                             #if os(iOS)
-                            Button {
-                                safariSheetURL = userProfileURL
-                            } label: {
-                                Label("Open Profile in Safari", systemImage: "safari")
-                            }
                             Divider()
+
                             Button {
                                 shareSheetURL = userProfileURL
                             } label: {
@@ -197,30 +207,32 @@ struct UserView: View {
                             deleteMenu
                         }
 
-                        #if os(iOS)
-                        Button {
-                            shareSheetURL = userProfileURL
-                        } label: {
-                            Label("Share", systemImage: "square.and.arrow.up")
-                        }
-                        #endif
+                        if let userProfileURL = userProfileURL {
+                            #if os(iOS)
+                            Button {
+                                shareSheetURL = userProfileURL
+                            } label: {
+                                Label("Share", systemImage: "square.and.arrow.up")
+                            }
+                            #endif
 
-                        Link(destination: userProfileURL) {
-                            Label("Open Profile", systemImage: "safari")
-                        }
-                        #if os(iOS)
-                        .contextMenu {
                             Link(destination: userProfileURL) {
                                 Label("Open Profile", systemImage: "safari")
                             }
+                            #if os(iOS)
+                            .contextMenu {
+                                Link(destination: userProfileURL) {
+                                    Label("Open Profile", systemImage: "safari")
+                                }
 
-                            Button {
-                                safariSheetURL = userProfileURL
-                            } label: {
-                                Label("Open Profile in Safari", systemImage: "safari")
+                                Button {
+                                    safariSheetURL = userProfileURL
+                                } label: {
+                                    Label("Open Profile in Safari", systemImage: "safari")
+                                }
                             }
+                            #endif
                         }
-                        #endif
 
                         #if os(macOS)
                         refreshButton
@@ -276,7 +288,6 @@ struct UserView: View {
 
     init(userID: String) {
         self.userID = userID
-        self.userProfileURL = .init(string: "https://twitter.com/intent/user?user_id=\(userID)")!
 
         self._users = FetchRequest(fetchRequest: {
             let fetchRequest = User.fetchRequest()
