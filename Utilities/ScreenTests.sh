480d6c2ac233e1a50f5ff4ccce1b2053761e9f09
Jaehong Kang <sinoru@me.com>
2022-03-22 22:11:24 +0900

    Add set -e, Add iPhone SE (3rd gen.)

--- Utilities/ScreenTests.sh
+++ Utilities/ScreenTests.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -e
+
 IOS_DEVICES=(
     'iPhone 13 Pro Max' # 6.5 Inch
     'iPhone 13' # 5.8 Inch
     'iPhone 8 Plus' # 5.5 Inch
+    'iPhone SE (3rd generation)' # 4.7 Inch
     'iPad Pro (12.9-inch) (5th generation)' # 12.9 Inch, iPad Pro (3rd generation)
     'iPad Pro (11-inch) (3rd generation)' # 11 Inch
     'iPad Pro (12.9-inch) (2nd generation)' # 12.9 Inch, iPad Pro (2nd generation)
