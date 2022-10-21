94503492d69b47097bdf05b65d7ddb5e5f4593ba
Jaehong Kang <sinoru@me.com>
2022-10-21 21:52:26 +0900

    Using NSPersistentCloudKitContainer.Event instead of PersistentContainer.CloudKitEvent

--- TweetNestKit/TweetNestKit/CoreDataStack/PersistentContainer.swift
+++ TweetNestKit/TweetNestKit/CoreDataStack/PersistentContainer.swift
@@ -36,7 +36,7 @@ public final class PersistentContainer: NSPersistentCloudKitContainer {
     #endif
 
     @MainActor @Published
-    public private(set) var cloudKitEvents: OrderedDictionary<UUID, PersistentContainer.CloudKitEvent> = [:]
+    public private(set) var cloudKitEvents: OrderedDictionary<UUID, NSPersistentCloudKitContainer.Event> = [:]
 
     init(inMemory: Bool = false, persistentStoreOptions: [String: Any?]? = nil) {
         super.init(name: "\(Bundle.tweetNestKit.name!).V3", managedObjectModel: Self.V3.managedObjectModel)
@@ -76,10 +76,10 @@ public final class PersistentContainer: NSPersistentCloudKitContainer {
         NotificationCenter.default
             .publisher(for: NSPersistentCloudKitContainer.eventChangedNotification, object: self)
             .compactMap { $0.userInfo?[NSPersistentCloudKitContainer.eventNotificationUserInfoKey] as? NSPersistentCloudKitContainer.Event }
-            .scan(OrderedDictionary<UUID, PersistentContainer.CloudKitEvent>()) {
+            .scan(OrderedDictionary<UUID, NSPersistentCloudKitContainer.Event>()) {
                 var cloudKitEvents = $0
 
-                cloudKitEvents[$1.identifier] = CloudKitEvent($1)
+                cloudKitEvents[$1.identifier] = $1
 
                 return cloudKitEvents
             }
@@ -118,80 +118,5 @@ public final class PersistentContainer: NSPersistentCloudKitContainer {
         backgroundContext.automaticallyMergesChangesFromParent = true
         backgroundContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
         return backgroundContext
-    }
-}
-
-extension PersistentContainer {
-    public struct CloudKitEvent {
-        public enum EventType {
-            case setup
-            case `import`
-            case export
-            case unknown(Int)
-        }
-
-        public var identifier: UUID
-        public var storeIdentifier: String
-        public var type: EventType
-        public var startDate: Date
-        public var endDate: Date?
-        public var result: Result<Void, Error>?
-    }
-}
-
-extension PersistentContainer.CloudKitEvent.EventType {
-    init(_ eventType: NSPersistentCloudKitContainer.EventType) {
-        switch eventType {
-        case .setup:
-            self = .setup
-        case .import:
-            self = .import
-        case .export:
-            self = .export
-        @unknown default:
-            self = .unknown(eventType.rawValue)
-        }
-    }
-}
-
-extension PersistentContainer.CloudKitEvent.EventType: Equatable { }
-
-extension PersistentContainer.CloudKitEvent {
-    init(_ event: NSPersistentCloudKitContainer.Event) {
-        self.identifier = event.identifier
-        self.storeIdentifier = event.storeIdentifier
-        self.type = EventType(event.type)
-        self.startDate = event.startDate
-        self.endDate = event.endDate
-
-        if let error = event.error {
-            result = .failure(error)
-        } else if event.succeeded {
-            result = .success(())
-        } else {
-            result = nil
-        }
-    }
-}
-
-extension PersistentContainer.CloudKitEvent: Equatable {
-    public static func == (lhs: PersistentContainer.CloudKitEvent, rhs: PersistentContainer.CloudKitEvent) -> Bool {
-        lhs.identifier == rhs.identifier &&
-        lhs.storeIdentifier == rhs.storeIdentifier &&
-        lhs.type == rhs.type &&
-        lhs.startDate == rhs.startDate &&
-        lhs.endDate == rhs.endDate &&
-        { () -> Bool in
-            switch (lhs.result, rhs.result) {
-            case (.success, .success):
-                return true
-            case (.failure, .failure):
-                return true
-            case (nil, nil):
-                return true
-            default:
-                return false
-            }
-        }()
     }
 }
