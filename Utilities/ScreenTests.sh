2f638b4269b944f2e2e6c35f0b312ff6b879bb64
Jaehong Kang <sinoru@me.com>
2022-10-24 03:50:56 +0900

    Update ScreenTests.sh

--- Utilities/ScreenTests.sh
+++ Utilities/ScreenTests.sh
@@ -13,46 +13,40 @@ IOS_DEVICES=(
 )
 
 WATCHOS_DEVICES=(
-    'Apple Watch Series 8 - 45mm' # Series 8 (45mm)
+    'Apple Watch Series 8 (45mm)' # Series 8 (45mm)
 )
 
 #######################################################################################################################
 
-xcodebuild -scheme "TweetNest" -testPlan "ScreenTests" -destination 'generic/platform=macOS' build-for-testing
-xcodebuild -scheme "TweetNest" -testPlan "ScreenTests" -destination 'generic/platform=iOS Simulator' build-for-testing
-xcodebuild -scheme "TweetNest Watch App" -testPlan "ScreenTests" -destination 'generic/platform=watchOS Simulator' build-for-testing
+# xcodebuild -scheme "TweetNest" -testPlan "ScreenTests" -destination 'platform=macOS' -resultBundlePath 'ScreenTests (macOS).xcresult' test
 
 #######################################################################################################################
 
-xcodebuild -scheme "TweetNest" -testPlan "ScreenTests" -destination 'platform=macOS' -resultBundlePath 'ScreenTests (macOS).xcresult' test-without-building
+# ios_destinations=()
+# for ios_device in "${IOS_DEVICES[@]}"
+# do
+#     xcrun simctl shutdown "$ios_device" >/dev/null 2>/dev/null || true
+#     xcrun simctl boot "$ios_device"
+#     xcrun simctl status_bar "$ios_device" override\
+#         --time '9:41 AM'\
+#         --dataNetwork 'wifi'\
+#         --wifiMode 'active'\
+#         --wifiBars 3\
+#         --cellularMode 'active'\
+#         --cellularBars 4\
+#         --operatorName ''\
+#         --batteryState 'discharging'\
+#         --batteryLevel 100
 
-#######################################################################################################################
+#     ios_destinations+=("-destination" "platform=iOS Simulator,name=$ios_device")
+# done
 
-ios_destinations=()
-for ios_device in "${IOS_DEVICES[@]}"
-do
-    xcrun simctl shutdown "$ios_device" >/dev/null 2>/dev/null || true
-    xcrun simctl boot "$ios_device"
-    xcrun simctl status_bar "$ios_device" override\
-        --time '9:41 AM'\
-        --dataNetwork 'wifi'\
-        --wifiMode 'active'\
-        --wifiBars 3\
-        --cellularMode 'active'\
-        --cellularBars 4\
-        --operatorName ''\
-        --batteryState 'discharging'\
-        --batteryLevel 100
+# xcodebuild -scheme "TweetNest" -testPlan "ScreenTests" "${ios_destinations[@]}" -resultBundlePath 'ScreenTests (iOS).xcresult' test
 
-    ios_destinations+=("-destination" "platform=iOS Simulator,name=$ios_device")
-done
-
-xcodebuild -scheme "TweetNest" -testPlan "ScreenTests" "${ios_destinations[@]}" -resultBundlePath 'ScreenTests (iOS).xcresult' test-without-building
-
-for ios_device in "${IOS_DEVICES[@]}"
-do
-    xcrun simctl shutdown "$ios_device"
-done
+# for ios_device in "${IOS_DEVICES[@]}"
+# do
+#     xcrun simctl shutdown "$ios_device"
+# done
 
 #######################################################################################################################
 
@@ -62,4 +56,4 @@ do
     watchos_destinations+=("-destination" "platform=watchOS Simulator,name=$watchos_device")
 done
 
-xcodebuild -scheme "TweetNest Watch App" -testPlan "ScreenTests" "${watchos_destinations[@]}" -resultBundlePath 'ScreenTests (watchOS).xcresult' test-without-building
+xcodebuild -scheme "TweetNest Watch App" -testPlan "ScreenTests" "${watchos_destinations[@]}" -resultBundlePath 'ScreenTests (watchOS).xcresult' test
